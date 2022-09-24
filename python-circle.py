f5bae1ab264ad3557c13c309b5ee854ebed30143
Seb Doe <39373619+sd0e@users.noreply.github.com>
2022-09-24 14:38:25 +0100

    Increase efficiency of the code

--- python-circle.py
+++ python-circle.py
@@ -4,8 +4,4 @@ diameter = int(input('diameter (even): '))
 thickness = diameter * thicknessFactor
 for i in range(diameter + 1):
     for j in range(diameter + 1):
-        if (i-diameter//2)**2 + (j-diameter//2)**2 - thickness <= (diameter/2)**2 <= (i-diameter//2)**2 + (j-diameter//2)**2 + thickness:
-            print('**', end="")
-        else:
-            print('  ', end="")
-    print('')
+        print('**' if (i-diameter//2)**2 + (j-diameter//2)**2 - thickness <= (diameter/2)**2 <= (i-diameter//2)**2 + (j-diameter//2)**2 + thickness else '  ', end="\n" if j == diameter else "")
