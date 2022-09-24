86044c7581ea9a66d4d9f5584f0ee36764f882aa
Seb Doe <39373619+sd0e@users.noreply.github.com>
2022-09-24 14:52:25 +0100

    Update python-circle.py

--- python-circle.py
+++ python-circle.py
@@ -1,7 +1,9 @@
-thicknessFactor = 0.5
+thicknessFactor = 2
+weight = 2 # 0-3
 
 diameter = int(input('diameter (even): '))
 thickness = diameter * thicknessFactor
+character = [' ', '.', '*', 'X'][weight]
 for i in range(diameter + 1):
     for j in range(diameter + 1):
-        print('**' if (i-diameter//2)**2 + (j-diameter//2)**2 - thickness <= (diameter/2)**2 <= (i-diameter//2)**2 + (j-diameter//2)**2 + thickness else '  ', end="\n" if j == diameter else "")
+        print(f'{character} ' if (i-diameter//2)**2 + (j-diameter//2)**2 - thickness <= (diameter/2)**2 <= (i-diameter//2)**2 + (j-diameter//2)**2 + thickness else '  ', end="\n" if j == diameter else "")
